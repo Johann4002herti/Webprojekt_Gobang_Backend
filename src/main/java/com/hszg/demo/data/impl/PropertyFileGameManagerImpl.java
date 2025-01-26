45758bdd5f813b4cce93716d4fc37c68536036ec
Johann Hertrampf <Johann.Hertrampf@stud.hszg.de>
2025-01-26 22:17:21 +0100

    fixing game

--- src/main/java/com/hszg/demo/data/impl/PropertyFileGameManagerImpl.java
+++ src/main/java/com/hszg/demo/data/impl/PropertyFileGameManagerImpl.java
@@ -136,7 +136,7 @@ public class PropertyFileGameManagerImpl implements GameManager {
         Game g = new Game("Test",15,"Gobang", true);
         PropertyFileGameManagerImpl fileGameManager = getPropertyFileGameManagerImpl("src/main/resources/GameList.properties");
 
-        //fileGameManager.storeGame(g);
+        fileGameManager.storeGame(g);
         System.out.println(fileGameManager.getGame(g.getGameCode()));
         fileGameManager.deleteGame(g.getGameCode());
 
